8ee2825a9c12bafd79b23bf07dd5236e6fa5f746
Alan Vieceli <alan.vieceli@softplan.com.br>
2019-11-28 15:41:41 -0300

    Validações

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -5,12 +5,20 @@ namespace exemplo_mvc_form.Controllers
 {
     public class ProductController : Controller
     {
-
+      [HttpGet]
       public IActionResult Save(){
         return View();
       }
 
+      [HttpPost]
       public IActionResult Save(Product product){
+
+        //if ((product.Id == 0) || string.IsNullOrEmpty(product.Name) || (product.Price == 0)){
+        if (!ModelState.IsValid){ //isso caso usar o [Required] no DTO
+          ViewBag.Validacao = "Produto inválido para cadastro";
+          //ViewBag é um utilitario da classe controller
+        }
+
         return View();
       }
         
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace exemplo_mvc_form.Models
 {
     public class Product
     {
 
+      [Required(ErrorMessage = "id é obrigatório")]
+      [Range(10,20, ErrorMessage = "O valor do campo ID deve estar entre 10 e 20.")]
       public int Id { get; set; }
-      public int Name { get; set; }
+      [Required]
+      [MinLength(3)]
+      [MaxLength(15)]
+      public string Name { get; set; }
+      [Required]
+      [Range(10,int.MaxValue)]
       public int Price { get; set; }
         
     }
